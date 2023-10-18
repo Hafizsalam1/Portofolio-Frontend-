2e16cbba520dc990550fb04d8f47ecfccb5a35a7
generalusersa <generalusersa@gmail.com>
2023-10-18 11:34:37 +0700

    feat adding animation in modal and contact page

--- src/Util/Alert.js
+++ src/Util/Alert.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
-import "./Alert.css"; 
+import "./Alert.css";
+import correct from "../Assets/correct.json";
+import incorrect from "../Assets/false.json"
+import Lottie from 'lottie-react';
 
-const Alert = ({ message, onClose }) => {
+const Alert = ({ message, onClose, animation }) => {
+  let animate;
+
+
+  if(animation == "correct"){
+     animate =  correct;
+  } else if(animation == "false"){
+    animate = incorrect;
+  }
+
   return (
     <div className="popup-container">
       <div className="popup">
         <div className="popup-content">
           <p>{message}</p>
+          <div style={{ width: '190px', height: '190px', paddingLeft: '100px' }}>
+          <Lottie
+            animationData={animate}
+            loop={false} 
+            autoplay={true} 
+          />
+          </div>
         </div>
         <button onClick={onClose}>Close</button>
       </div>
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Styles from "./ContactStyle";
 import Alert from "../../Util/Alert";
 
 const Contact = () => {
+  const [animationContext, setAnimationContext] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
+  const [formIsValid,setFormIsValid] = useState(true)
+
+
 
   const [showPopup, setShowPopup] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
@@ -19,8 +23,26 @@ const Contact = () => {
     setShowPopup(true);
   }
 
+
+  const [isAnimated, setIsAnimated] = React.useState(false);
+
+  useEffect(() => {
+    const animationTimeout = setTimeout(() => {
+      setIsAnimated(true);
+    }, 200);
+
+    return () => clearTimeout(animationTimeout);
+  }, []);
+
   const handleChange = (setState) => (e) => {
     setState(e.target.value);
+    if (!name || !email || !phone || !message) {
+      setFormIsValid(false);
+      return;
+    }else{
+      setFormIsValid(true);
+    }
+
   };
 
   let messageData = {
@@ -34,6 +56,10 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!name || !email || !phone || !message) {
+      setFormIsValid(false);
+      return;
+    }
     try {
       messageData = {
         name: name,
@@ -55,18 +81,25 @@ const Contact = () => {
       }
 
       const data = await response.json();
+      setAnimationContext("correct")
       setAlertMessage("Message Sent")
       setShowPopup(true);
       setResponse(data);
     } catch (error) {
-      setAlertMessage("Message Not Sent")
+      setAlertMessage("Message failed to send")
+      setAnimationContext("false")
       setShowPopup(true);
       console.error('Error:', error);
     }
   };
 
   return (
-    <div style={Styles.container}>
+    <div style={{
+      ...Styles.container,
+      opacity: isAnimated ? 1 : 0, 
+      transform: isAnimated ? "none" : "translateY(20px)", 
+      transition: "opacity 1s ease, transform 1s ease", 
+    }}>
       <div>
         <h1 style={Styles.AboutMe}>Contact Me</h1>
         <form style={Styles.form} onSubmit={handleSubmit}>
@@ -74,7 +107,7 @@ const Contact = () => {
           <input type="text" value={name} onChange={handleChange(setName)} style={Styles.formBox} /><br />
           <span style={Styles.ketForm}>email</span><br />
           {showPopup && (
-        <Alert message={alertMessage} onClose={onCloseAlert} />
+        <Alert message={alertMessage} onClose={onCloseAlert} animation={animationContext}/>
       )}
           <input type="email" value={email} onChange={handleChange(setEmail)} style={Styles.formBox} /><br />
           <span style={Styles.ketForm}>phone</span><br />
@@ -82,6 +115,11 @@ const Contact = () => {
           <span style={Styles.ketForm}>message</span><br />
           <textarea value={message} onChange={handleChange(setMessage)} style={Styles.formBoxKeterangan}></textarea><br /><br /><br /><br />
           <button style={Styles.formBox} type="submit">Send</button>
+          {!formIsValid && (
+  <div style={{ marginTop: '-50px' }}>
+    <p style={{ color: 'red', fontSize: '16px' }}>*Mohon isi semua field</p>
+  </div>
+)}
         </form>
       </div>
     </div>
